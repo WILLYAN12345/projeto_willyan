aae62e822087b7f164e9c65a5ada808a40bd62e3
WILLYAN12345 <161588690+WILLYAN12345@users.noreply.github.com>
2024-04-30 08:42:26 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,30 +10,30 @@ meuFormulario.addEventListener("submit", function(event) {
 
 //Função divisao
 function divisao() {
-  
+
   //Captura o que está dentro do primeiro input no formulário
   //Number - Transforma o texto capturado em Numero
-  let primeiro_numero_inteiro = Number(document.getElementById("primeiro_numero_inteiro").value);
-  
+  let primeira_nota = Number(document.getElementById("primeira_nota").value);
+
   //Captura o que está dentro do segundo input no formulário
   //Number - Transforma o texto capturado em Numero
-  let segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value); 
-  
+  let segunda_nota = Number(document.getElementById("segunda_nota").value);
+
    //Captura o que está dentro do terceiro input no formulário
   //Number - Transforma o texto capturado em Numero
-  let terceiro_numero_inteiro = Number(document.getElementById("terceiro_numero_inteiro").value); 
-  
+  let terceira_nota = Number(document.getElementById("terceira_nota").value);
+
    //Captura o que está dentro do quarto input no formulário
   //Number - Transforma o texto capturado em Numero
-  let quarto_numero_inteiro = Number(document.getElementById("quarto_numero_inteiro").value); 
-  
+  let quarta_nota = Number(document.getElementById("quarta_nota").value);
+
   //Realiza a divisao do valor que está na variável "primeiro_numero_inteiro" com o valor que está na
   //variável "segundo_numero_inteiro"
   //E a variável "resultado_divisao" recebe o valor da divisao
-  let resultado_soma = primeiro_numero_inteiro / segundo_numero_inteiro / terceiro_numero_inteiro / quarto_numero_inteiro;
+  let resultado_divisao = ( primeira_nota + segunda_nota + terceira_nota + quarta_nota ) / 4;
 
   //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_divisao"
   document.getElementById("resultado_divisao").value = resultado_divisao;
-  
-  
+
+
 }
