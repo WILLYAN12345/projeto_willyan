89e83745c8eadd7707084ff93ff55b7dbc4a1185
WILLYAN12345 <161588690+WILLYAN12345@users.noreply.github.com>
2024-06-19 09:09:16 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,43 +9,27 @@ event.preventDefault();
 
 
 //Função somar
-  function multiplicar (num1, num2) {
-      let produto = num1 * num2;
-      if (produto % 2 === 0) {
-        return true;
-      } else {
-        return false;
-      }
+  function multiplicar () {
+
+    let numero1 = Number(document.getElementById("primeiro_numero").value);
+
+    console.log(numero1);
 
 
-  // Exemplo de uso
-  let numero1 = Number(document.getElementById("primeiro_numero_par"));
+    let numero2 = Number(document.getElementById("segundo_numero").value);
 
-  let numero2 = Number(document.getElementById("segundo_numero_par"));
+    console.log(numero2);
 
-  if (multiplicar(numero1, numero2)) {
-      console.log("O produto é par.");
-  } else {
-      console.log("O produto não é par.");
-  }
-}
-/*
-  //Captura o que está dentro do primeiro input no formulário
-  //Number - Transforma o texto capturado em Numero
-  let verificar se o primeiro numero é par = Number(document.getElementById("primeiro numero par").value);
+    let produto = numero1 * numero2;
 
-  //Captura o que está dentro do segundo input no formulário
-  //Number - Transforma o texto capturado em Numero
-  let verificar se o segundo numero é par = Number(document.getElementById("segundo numero par").value);
+    console.log(produto);
 
-  //Realiza a soma do valor que está na variável "primeiro_numero_inteiro" com o valor que está na
-  //variável "segundo_numero_inteiro"
-  //E a variável "resultado_soma" recebe o valor da soma
-  let resultado_soma = primeiro_numero_inteiro + segundo_numero_inteiro;
-
-  //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_soma"
-  document.getElementById("resultado_soma").value = resultado_soma;
-
+    //verificar se o produto de dois numeros é par
+    if (produto % 2 == 0 ) {
+      console.log("é par");
+      document.getElementById("resultado").value = "é par";
+    } else {
+      document.getElementById("resultado").value = "é impar";
+    }
 
 }
-*/
